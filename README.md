a55a25bb7d85782c3c8eb1040fd8cbb780cbee08
i0x0i <ivan.i0x0i@gmail.com>
2016-07-29 16:32:44 +0800

    add script for rpi

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 #Install
+##Ubuntu 16.04
 curl https://raw.githubusercontent.com/I0x0I/humanoid_navigation/rosinstall/install.sh | sh
+##Ubuntu 16.04 for Raspberry Pi
+curl https://raw.githubusercontent.com/I0x0I/humanoid_navigation/rosinstall/install-rpi.sh | sh
--- install.sh
+++ install.sh
@@ -6,8 +6,9 @@ rosdep update
 mkdir ~/ros_catkin_ws
 cd ~/ros_catkin_ws/
 apt-get install -y python-nose cmake python-dev libboost-all-dev python-empy python-empy libtinyxml-dev libgtest-dev python-numpy sbcl python-imaging libbz2-dev pkg-config libaprutil1-dev python-coverage liblog4cxx10-dev python-matplotlib python-matplotlib python-wxgtk3.0 tango-icon-theme libeigen3-dev libcollada-dom2.4-dp-dev python-sip-dev shiboken libpyside-dev liburdfdom-dev python-qt4-dev unzip python-qt4-gl python-psutil libpoco-dev libyaml-cpp-dev python-pygraphviz libqhull-dev liblz4-dev python-pydot python-opencv hddtemp python-pyside libogre-1.9-dev libogre-1.9-dev python-opengl libassimp-dev libshiboken-dev libcurl4-openssl-dev python-netifaces libopencv-dev libcppunit-dev python-paramiko python-qwt5-qt4 libpcl1.7 libpcl-dev libsdl-image1.2-dev libvtk-java python-vtk6 libproj-dev libproj9 libproj-java
-curl -o indigo.rosinstall https://raw.githubusercontent.com/I0x0I/humanoid_navigation/rosinstall/indigo.rosinstall
-wstool init -j8 src indigo.rosinstall
+rosinstall_generator desktop --rosdistro indigo --deps --wet-only --tar > indigo-desktop-wet.rosinstall
+curl https://raw.githubusercontent.com/I0x0I/humanoid_navigation/rosinstall/indigo-humanoid-extra.rosinstall >> indigo-desktop-wet.rosinstall
+wstool init -j8 src indigo-desktop-wet.rosinstall
 mkdir sbpl/build
 cd sbpl/build
 cmake ..
